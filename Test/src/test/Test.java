1eef263b25859a56ac0804632491b7a7aa14512a
6 <6@6-PC>
2019-12-11 08:37:22 +0900

    커밋

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -7,25 +7,24 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-int a,b=0;
-double sum=0;
-Scanner scan=new Scanner(System.in);
-System.out.println("숫자");
-boolean run=true;
+		int a, b = 0;
+		double sum = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("숫자");
+		boolean run = true;
 
-while(run) {
-	a=scan.nextInt();
+		while (run) {
+			a = scan.nextInt();
+			if (a == 0) {
+				run = false;
+			} else {
+				b++;
+				sum += a;
+			}
+		}		
+		System.out.println(b);
+		System.out.println(sum / b);
 
-	
-	if(a==0) {
-		run=false;
-		
-	}else {
-		b++;
-		sum+=a;
-	}
-}System.out.println(sum/b);
-	
 	}
 
 }
---
+++ Test191210/src/test/Test5.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int a,b=0,c,sum=0;
+		c=scan.nextInt();
+		for(a=c;a==0;b++) sum+=b;{
+			System.out.println(sum/b);
+		}
+
+	}
+
+}
