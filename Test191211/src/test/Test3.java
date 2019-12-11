f673f0b1c71a1d082e3465c5a76beb4fd76abf43
6 <6@6-PC>
2019-12-11 16:58:50 +0900

    2019/12/11일차 1

--- Test191211/src/test/Test3.java
+++ Test191211/src/test/Test3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Test3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// up&down 범위는 1~100
 Scanner scan=new Scanner(System.in);
 int a,b,c=0;
 
