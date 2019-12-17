4f432e37fc73771d300a8a6a4e155e5597b23bac
6 <6@6-PC>
2019-12-17 10:17:22 +0900

    커밋

--- Test191217/src/test/Test.java
+++ Test191217/src/test/Test.java
@@ -1,14 +1,17 @@
 package test;
 
+import java.util.Scanner;
+
 public class Test {
+	
 	int gas;
 
 	void setGas(int gas) {
-		this.gas = gas;
+		this.gas = gas;//2.호출받아 가스값 받고 가스 필드에 값을 대입 무조건작동
 	}
 
 	boolean isLeftGas() {
-		if (gas == 0) {
+		if (gas == 0) {//4.호출받아 가스가0이면 가스값 없음 false, 0보다 많으면 있음 true를 보내줌 무조건작동
 			System.out.println("gas가 없습니다");
 			return false;
 		} else {
@@ -17,9 +20,21 @@ public class Test {
 		}
 	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	void run() {
-		while (true) {
-			if (gas > 0) {
+		while (true) {//6.호출받아   가스가 0될때까지 반복 값에따라 작동안할수도있음
+			if (gas >0) {
 				System.out.println("달립니다.(gas잔량:" + gas + ")");
 				gas -= 1;
 			} else {
@@ -28,5 +43,9 @@ public class Test {
 			}
 		}
 	}
-
+	
+	
+	
+	
+	
 }
--- Test191217/src/test/Testmain.java
+++ Test191217/src/test/Testmain.java
@@ -1,23 +1,14 @@
 package test;
 
-import java.util.Scanner;
+
 
 public class Testmain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-Test ts=new Test();
-Scanner scan=new Scanner(System.in);
-int input=scan.nextInt();
-ts.setGas(input);
-		boolean gasState=ts.isLeftGas();
-		System.out.println(ts.gas);
-		if(gasState) {
-			System.out.println("출발");
-			ts.run();
-		}
-		
-		
+
+Input in=new Input();
+in.input();				
 	}
 
 }
